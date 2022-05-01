799de6144c716f5194d1c4318ebe45ee804aa06c
Ameer Hamza <ameer.hamza1811@gmail.com>
2022-05-01 22:39:10 +0500

    build basic schema for graphql

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require("graphql");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res, next) => {
-    res.send("Hello World!");
-});
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+    type RootQuery {
+      events: [String!]!
+    }
+    type RootMutation {
+      createEvent(name: String): String
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+
+  `),
+    rootValue: {
+      events: () => {
+        return ["Event 1", "Event 2", "Event 3"];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
+    graphiql: true,
+  })
+);
 
 app.listen(5000, () => {
   console.log("Server started on port 3000");
